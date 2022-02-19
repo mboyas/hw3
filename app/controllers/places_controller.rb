270d14ed2c97bb471de96ede64f049cef09455aa
mboyas <michael.boyas@kellogg.northwestern.edu>
2022-02-19 18:52:32 +0000

    added new and create to places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,5 +7,15 @@ class PlacesController < ApplicationController
     def show
       @place = Place.find(params["id"])
     end
+
+    def new
+      @place = Place.new
+    end
+  
+    def create
+      @place = Place.new(params["place"])
+      @place.save
+      redirect_to "/places"
+    end
   
   end
